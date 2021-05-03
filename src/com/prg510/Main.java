f36efacf173a6d0971abb158b3559e3babaf7264
hkadnjimi-max <hermand180@gmail.com>
2021-05-03 08:35:59 +0200

    First repo update from IntelliJ

---
+++ src/com/prg510/Main.java
@@ -0,0 +1,11 @@
+package com.prg510;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Good day all, sets GIT");
+        System.out.println("We hope Git is cool!!!");
+        //Pull->Commit->Push
+    }
+}
